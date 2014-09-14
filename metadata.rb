1142455744b3cdd89011924b6d2c4440df51466e
Michiel <code@m01.eu>
2014-09-14 01:04:11 +0100

    metadata update

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'django_tutorial'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+maintainer       'Michiel'
+maintainer_email 'code@m01.eu'
+license          'MIT'
 description      'Installs/Configures django_tutorial'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
